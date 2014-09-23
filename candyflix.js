55f2024e035cbed7aa9dba08fbc14693e7bfe264
G-Ray <geoffrey.bonneville@gmail.com>
2014-09-24 00:09:35 +0200

    Start peerflix process on a free port

--- candyflix.js
+++ candyflix.js
@@ -1,5 +1,6 @@
 var io = require('socket.io')();
 var spawn = require('child_process').spawn;
+var getport = require('getport');
 
 io.on('connection', function(socket){
   console.log('a user connected');
@@ -12,16 +13,19 @@ io.on('connection', function(socket){
     console.log(msg);
     if(msg['torrent']) {
       console.log(msg.torrent.stream[0]);
-      //var peerflix = process.exec('peerflix ' + msg.torrent.stream[0] + ' --port 8086');
-      var child = spawn('peerflix', [msg.torrent.stream[0], '--port='+ 8080], {});
-      child.stdout.on('data', function(data) {
-        console.log('stdout: ' + data);
-      });
-      child.stderr.on('data', function(data) {
-          console.log('stderr: ' + data);
-      });
 
-      io.emit('streamUrl', 'http://localhost:8080/');
+      getport(function (err, port) {
+        if (err) console.log(err);
+        var child = spawn('peerflix', [msg.torrent.stream[0], '--port='+ port], {});
+        child.stdout.on('data', function(data) {
+          console.log('stdout: ' + data);
+        });
+        child.stderr.on('data', function(data) {
+            console.log('stderr: ' + data);
+        });
+
+        io.emit('streamUrl', 'http://localhost:' + port);
+      });
     }
   });
 });
