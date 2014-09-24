3cf89e0fe87706cc77d4e2af2ea5a9887c7ea322
G-Ray <geoffrey.bonneville@gmail.com>
2014-09-24 13:53:17 +0200

    Kill a stream when there is no spectator

--- candyflix.js
+++ candyflix.js
@@ -9,18 +9,32 @@ io.on('connection', function(socket){
 
   socket.on('disconnect', function(){
     console.log('user disconnected');
+
+    if(socket.playing) { // There was already a stream running
+      processes[socket.playing].spectators--;
+      if(processes[socket.playing].spectators === 0) {
+        processes[socket.playing].child.kill();
+        delete processes[socket.playing];
+      }
+    }
   });
 
   socket.on('msg', function(msg){
     console.log(msg);
     if(msg['torrent']) {
-      console.log(msg.torrent.stream[0]);
+      console.log(msg.torrent.stream[1]);
 
       getport(function (err, port) {
         if (err) console.log(err);
+
         if(!processes[msg.torrent.stream[1]]) {
+          var process = {};
           var child = spawn('peerflix', [msg.torrent.stream[0], '--port='+ port], {});
-          processes[msg.torrent.stream[1]] = port;
+          process.port = port;
+          process.spectators = 0;
+          process.child = child;
+
+          processes[msg.torrent.stream[1]] = process;
 
           child.stdout.on('data', function(data) {
             //console.log('stdout: ' + data);
@@ -28,10 +42,26 @@ io.on('connection', function(socket){
           child.stderr.on('data', function(data) {
             console.log('stderr: ' + data);
           });
+          child.on('close', function (code, signal) {
+            console.log('child process terminated due to receipt of signal '+signal);
+          });
         }
-        else port = processes[msg.torrent.stream[1]];
+        else port = processes[msg.torrent.stream[1]].port;
 
-        console.log("###RUNNING PROCESSES###");
+        processes[msg.torrent.stream[1]].spectators++;
+
+        console.log(socket.playing);
+        if(socket.playing) { // There was already a stream running
+          processes[socket.playing].spectators--;
+          if(processes[socket.playing].spectators === 0) {
+            processes[socket.playing].child.kill();
+            delete processes[socket.playing];
+          }
+        }
+
+        socket.playing = msg.torrent.stream[1];
+
+        console.log("##RUNNING PROCESSES##");
         console.log(processes);
         console.log("#####################");
 
