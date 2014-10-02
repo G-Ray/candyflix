0e76f8ab28a705b9a9176c92218c7f3a93bf7bdc
G-Ray <geoffrey.bonneville@gmail.com>
2014-10-02 22:53:50 +0200

    Move subtitles functions

--- app/candyflix.js
+++ app/candyflix.js
@@ -3,56 +3,9 @@ var spawn = require('child_process').spawn;
 var getport = require('getport');
 var request = require("request");
 
-var langcodes = require('./langcodes.js');
+var subtitles = require('./subtitles.js');
 
 var processes = {};
-
-// Decompress zip
-function decompress(dataBuff, callback) {
-  try {
-    console.log(dataBuff);
-    var AdmZip  = require('adm-zip');
-    var zip = new AdmZip(dataBuff);
-    var zipEntries = zip.getEntries();
-    console.log(zipEntries);
-    // TODO: Shouldn't we look for only 1 file ???
-    zipEntries.forEach(function(zipEntry, key) {
-      if (zipEntry.entryName.indexOf('.srt') != -1) {
-        var decompressedData = zip.readFile(zipEntry);
-        callback(decompressedData);
-      }
-    });
-  } catch (error) {
-    console.log('Failed to decompress subtitle!', error);
-  }
-}
-
-// Handles charset encoding
-function decode(dataBuff, language, callback) {
-  var charsetDetect = require('jschardet');
-  var targetEncodingCharset = 'utf8';
-
-  var charset = charsetDetect.detect(dataBuff);
-  var detectedEncoding = charset.encoding;
-  console.log("SUB charset detected: "+detectedEncoding);
-  // Do we need decoding?
-  if (detectedEncoding.toLowerCase().replace('-','') == targetEncodingCharset) {
-    callback(dataBuff.toString('utf-8'));
-  // We do
-  } else {
-    var iconv = require('iconv-lite');
-    var langInfo = langcodes[language] || {};
-    console.log("SUB charset expected: "+langInfo.encoding);
-    if (langInfo.encoding !== undefined && langInfo.encoding.indexOf(detectedEncoding) < 0) {
-      // The detected encoding was unexepected to the language, so we'll use the most common
-      // encoding for that language instead.
-      detectedEncoding = langInfo.encoding[0];
-    }
-    console.log("SUB charset used: "+detectedEncoding);
-    dataBuff = iconv.encode( iconv.decode(dataBuff, detectedEncoding), targetEncodingCharset );
-    callback(dataBuff.toString('utf-8'));
-  }
-}
 
 io.on('connection', function(socket){
   console.log('a user connected');
@@ -78,8 +31,8 @@ io.on('connection', function(socket){
         if (!error && response.statusCode == 200) {
           var path = require('path');
 
-          decompress(data, function(dataBuf) {
-              decode(dataBuf, msg['getSubs'][1], function(dataBuff) {
+          subtitles.decompress(data, function(dataBuf) {
+              subtitles.decode(dataBuf, msg['getSubs'][1], function(dataBuff) {
                 socket.emit('dataBuf', dataBuff);
               });
           });
